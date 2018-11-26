a89067ee53d4015f99d19d77b1e2dfecd3ef3687
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-11-25 21:28:14 -0500

    Example: Creating a Filter Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FilterStatusPipe } from './filter-status.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterStatusPipe
   ],
   imports: [
     BrowserModule,
